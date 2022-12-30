5800e37b3591b1d0c5ce76a1fcdd3c8124917fe1
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2022-12-30 23:26:55 +0100

    training with SDL

---
+++ SDL_utilities/color_related.c
@@ -0,0 +1,11 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int setWindowColor(SDL_Renderer *renderer, SDL_Color color){
+    if(SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a) < 0)
+        return -1;
+    if(SDL_RenderClear(renderer) < 0)
+        return -1;
+    return 0;  
+}
--- sdl_test.c
+++ sdl_test.c
@@ -1,24 +1,90 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "SDL_utilities/drawers.c"
+#include "SDL_utilities/color_related.c"
+
 
 int main(int argc, char *argv[]){
-    if(0 != SDL_Init(SDL_INIT_VIDEO))
-    {
+    SDL_Color orange = {255, 127, 40, 255};
+    if(0 != SDL_Init(SDL_INIT_VIDEO)){
         fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
         goto Quit;
     }
     SDL_Window* window= SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                               640, 480, SDL_WINDOW_SHOWN);
-    if(NULL == window)
-    {
+    if(NULL == window){
         fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
         goto Quit;
     }
+    SDL_Renderer* renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+    if(NULL == renderer){
+        fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
+        goto Quit;
+    }
+    if(0 != SDL_SetRenderDrawColor(renderer, orange.r, orange.g, orange.b, orange.a)){
+        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
+        goto Quit;
+    }
+
     /* On agit sur la fenêtre ici */
-    SDL_Delay(3000);
-    SDL_DestroyWindow(window);
+    SDL_RenderClear(renderer);
+    SDL_Delay(1000);
+    SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderDrawPoint(renderer, 50, 50);
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    SDL_RenderDrawPoint(renderer, 100, 100);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderDrawPoint(renderer, 150, 150);
+    DrawCircle(renderer,320,240,100);
+    SDL_RenderDrawLine(renderer,220,240,420,240);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(10000);
     Quit:
-    SDL_Quit();
+        if (renderer != NULL) SDL_DestroyRenderer(renderer);
+        if  (window != NULL) SDL_DestroyWindow(window);
+        SDL_Quit();
     return EXIT_SUCCESS;
+}
+
+void sdl_ellipse(SDL_Renderer* r, int x0, int y0, int radiusX, int radiusY){
+    float pi = 3.14159265358979323846264338327950288419716939937510;
+    float pih = pi / 2.0;
+    const int prec = 27; // precision value; value of 1 will draw a diamond, 27 makes pretty smooth circles.
+    float theta = 0;     // angle that will be increased each loop
+    int x  = (float)radiusX * cos(theta);//start point
+    int y  = (float)radiusY * sin(theta);//start point
+    int x1 = x;
+    int y1 = y;
+
+    //repeat until theta >= 90;
+    float step = pih/(float)prec; // amount to add to theta each time (degrees)
+    for(theta=step;  theta <= pih;  theta+=step)//step through only a 90 arc (1 quadrant)
+    {
+        //get new point location
+        x1 = (float)radiusX * cosf(theta) + 0.5; //new point (+.5 is a quick rounding method)
+        y1 = (float)radiusY * sinf(theta) + 0.5; //new point (+.5 is a quick rounding method)
+
+        //draw line from previous point to new point, ONLY if point incremented
+        if( (x != x1) || (y != y1) )//only draw if coordinate changed
+        {
+            SDL_RenderDrawLine(r, x0 + x, y0 - y,    x0 + x1, y0 - y1 );//quadrant TR
+            SDL_RenderDrawLine(r, x0 - x, y0 - y,    x0 - x1, y0 - y1 );//quadrant TL
+            SDL_RenderDrawLine(r, x0 - x, y0 + y,    x0 - x1, y0 + y1 );//quadrant BL
+            SDL_RenderDrawLine(r, x0 + x, y0 + y,    x0 + x1, y0 + y1 );//quadrant BR
+        }
+        //save previous points
+        x = x1;//save new previous point
+        y = y1;//save new previous point
+    }
+    //arc did not finish because of rounding, so finish the arc
+    if(x!=0)
+    {
+        x=0;
+        SDL_RenderDrawLine(r, x0 + x, y0 - y,    x0 + x1, y0 - y1 );//quadrant TR
+        SDL_RenderDrawLine(r, x0 - x, y0 - y,    x0 - x1, y0 - y1 );//quadrant TL
+        SDL_RenderDrawLine(r, x0 - x, y0 + y,    x0 - x1, y0 + y1 );//quadrant BL
+        SDL_RenderDrawLine(r, x0 + x, y0 + y,    x0 + x1, y0 + y1 );//quadrant BR
+    }
 }
