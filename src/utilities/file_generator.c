e576372b07ed92fd9c5c327ff826264be3b5dc44
Esteban <estebanmandjukowski@gmail.com>
2023-01-24 23:16:54 +0100

    added test sets,working on readme

--- src/utilities/file_generator.c
+++ src/utilities/file_generator.c
@@ -1,29 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+/*
+Input : ./file_generator filename n low high
+
+- filename : filename of output
+- n : number of 2D coordinates points you wanna get.
+- low_x high_x low_y high_y : interval for which random coordinates will emerge from
+*/
+
 int rand_between(int l, int r) {
   return (int)( (rand() / (RAND_MAX * 1.0f)) * (r - l) + l);
 }
 
 
-void write_file(FILE* f,int n,int l,int r){
+void write_file(FILE* f,int n,int l_x,int h_x,int l_y,int h_y){
     int temp_1,temp_2;
     fprintf(f,"%d\n",n);
     for (int i = 0; i < n;i++){
-        temp_1 = rand_between(l,r);
-        temp_2 = rand_between(l,r);
+        temp_1 = rand_between(l_x,h_x);
+        temp_2 = rand_between(l_y,h_y);
         fprintf(f,"%d;%d\n",temp_1,temp_2);
     }
 }
 
 int main(int argc,char* argv[]){
-    if (argc != 5) return EXIT_FAILURE;
+    if (argc != 7) return EXIT_FAILURE;
     FILE* out_f = fopen(argv[1],"w");
     int n = atoi(argv[2]);
-    int low_bound = atoi(argv[3]);
-    int high_bound = atoi(argv[4]);
-    write_file(out_f,n,low_bound,high_bound);
+    int low_x_bound = atoi(argv[3]);
+    int high_x_bound = atoi(argv[4]);
+    int low_y_bound = atoi(argv[5]);
+    int high_y_bound = atoi(argv[6]);
+    write_file(out_f,n,low_x_bound,high_x_bound,low_y_bound,high_y_bound);
     return 0;
 }
-
-//gcc file_generator.c -o fg -Wall -Wvla -Wextra -fsanitize=address
